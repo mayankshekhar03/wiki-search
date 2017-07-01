695484f432c3400e46f71baaeaedce7bf3888826
Mayank Shekhar <mayankshekhar03@gmail.com>
2017-07-01 17:31:46 +0530

    finished building the skeleton

---
+++ extras/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".query").addClass("animated bounceIn");
+    document.getElementById("search-input").focus();
+    //onclick #search
+    //call input handler and creates a link
+        // calls the results manipulater by passing the link
+    //onclick rendom search
+        //opens a random wikiarticle
+});

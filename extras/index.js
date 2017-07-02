de68e3dcae48387377489d87b278116faf08ab7a
Mayank Shekhar <mayankshekhar03@gmail.com>
2017-07-02 11:59:51 +0530

    got everything to work except search

--- extras/index.js
+++ extras/index.js
@@ -1,9 +1,24 @@
+
+
 $(document).ready(function(){
-    $(".query").addClass("animated bounceIn");
-    document.getElementById("search-input").focus();
-    //onclick #search
-    //call input handler and creates a link
-        // calls the results manipulater by passing the link
-    //onclick rendom search
-        //opens a random wikiarticle
+   $("#search-button").click(function(){
+    $.ajax({
+          url: 'http://en.wikipedia.org/w/api.php',
+          data: { action: 'query', list: 'search', srsearch: 'lucknow', format: 'json' },
+          dataType: 'jsonp',
+          success: function (x) {
+              for (var i = 0; i < x.query.search.length; i++){
+                $(".results").append('<a href="https://en.wikipedia.org/wiki/' + x.query.search[i].title + '">Open</a><br>' + x.query.search[i].snippet + '<hr>');
+              }
+          }
+    });
+    });
+      /*  $.ajax({
+          url: 'http://en.wikipedia.org/w/api.php',
+          data: { action: 'query', list: 'search', srsearch: 'Richard Feynman', format: 'json' },
+          dataType: 'jsonp',
+          success: function (x) {
+            $(".results").append(x.query.search[0].snippet);
+          }
+        }); */
 });

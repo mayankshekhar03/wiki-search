ae4c5d3116be08a2de1db08cbc5ca6350e8fa156
Mayank Shekhar <mayankshekhar03@gmail.com>
2017-07-02 13:39:09 +0530

    finally the data is returning from the api, big relief

--- extras/index.js
+++ extras/index.js
@@ -1,24 +1,18 @@
 
 
 $(document).ready(function(){
-   $("#search-button").click(function(){
-        var q = $('#search-input').val();
+   $("#search-button").click(function(e){
+        e.preventDefault();  // this shit was causing the api to not load any results wtf
+        q = $('#search-input').val();
         $.ajax({
-              url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + q + "&prop=info&inprop=url&utf8=&format=json",
-              dataType: 'jsonp',
-              success: function (x) {
-                  for (var i = 0; i < x.query.search.length; i++){
-                    $(".results").append('<a href="https://en.wikipedia.org/wiki/' + x.query.search[i].title + '">Open</a><br>' + x.query.search[i].snippet + '<hr>');
-                  }
+            url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + q.toString() + "&prop=info&inprop=url&utf8=&format=json",
+            dataType: 'jsonp',
+            success: function (x) {
+              for (var i = 0; i < x.query.search.length; i++){
+                $(".results").append('<a href="https://en.wikipedia.org/wiki/' + x.query.search[i].title + '">Open</a><br>' + x.query.search[i].snippet + '<hr>');
               }
+            },
+            error: function(){alert('error retrieving data.');}
         });
     });
-      /*  $.ajax({
-          url: 'http://en.wikipedia.org/w/api.php',
-          data: { action: 'query', list: 'search', srsearch: 'Richard Feynman', format: 'json' },
-          dataType: 'jsonp',
-          success: function (x) {
-            $(".results").append(x.query.search[0].snippet);
-          }
-        }); */
 });

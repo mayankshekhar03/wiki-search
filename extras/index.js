f90e3d374832d61dea5934869ea5bd1a2808be84
Mayank Shekhar <mayankshekhar03@gmail.com>
2017-07-02 13:13:18 +0530

    huh

--- extras/index.js
+++ extras/index.js
@@ -2,16 +2,16 @@
 
 $(document).ready(function(){
    $("#search-button").click(function(){
-    $.ajax({
-          url: 'http://en.wikipedia.org/w/api.php',
-          data: { action: 'query', list: 'search', srsearch: 'lucknow', format: 'json' },
-          dataType: 'jsonp',
-          success: function (x) {
-              for (var i = 0; i < x.query.search.length; i++){
-                $(".results").append('<a href="https://en.wikipedia.org/wiki/' + x.query.search[i].title + '">Open</a><br>' + x.query.search[i].snippet + '<hr>');
+        var q = $('#search-input').val();
+        $.ajax({
+              url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + q + "&prop=info&inprop=url&utf8=&format=json",
+              dataType: 'jsonp',
+              success: function (x) {
+                  for (var i = 0; i < x.query.search.length; i++){
+                    $(".results").append('<a href="https://en.wikipedia.org/wiki/' + x.query.search[i].title + '">Open</a><br>' + x.query.search[i].snippet + '<hr>');
+                  }
               }
-          }
-    });
+        });
     });
       /*  $.ajax({
           url: 'http://en.wikipedia.org/w/api.php',
